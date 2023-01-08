7770bae1567f8d4927a0c68d8271aa22fcdd27f3
Jun Kim <jk9eem@virginia.edu>
2023-01-07 21:59:59 -0500

    base setup

---
+++ main.js
@@ -0,0 +1,8 @@
+// 1. create 2 boxes
+// 2. create dropdown list
+// 3. get the currency exchange rate
+// 4. item changes when select the item from the dropdown list
+// 5. get the currency exchange amount after inputting the amount
+// 6. the amount is exchanged based on the item rate that selected from the list
+// 7. put the currency name
+// 8. works top to bottom and vise versa
