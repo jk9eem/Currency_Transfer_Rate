c7a61e3b9cd0e1a6de492b20b2222c446d3f516f
Jun Kim <jk9eem@virginia.edu>
2023-01-09 22:02:03 -0500

    set currency ratio and button actions

--- main.js
+++ main.js
@@ -7,3 +7,48 @@
 // 7. put the currency name
 // 8. works top to bottom and vise versa
 
+let currencyRatio = {
+  USD: {
+    EUR: 0.93,
+    GBP: 0.82,
+    USD: 1,
+    unit: "dollar",
+  },
+
+  EUR: {
+    GBP: 0.88,
+    USD: 1.07,
+    EUR: 1,
+    unit: "euro",
+  },
+
+  GBP: {
+    USD: 1.22,
+    EUR: 1.13,
+    GBP: 1,
+    unit: "pound",
+  },
+};
+
+let fromCurrency = 'USD';
+let toCurrency = 'USD';
+
+// button action -> click event (for from currency)
+document.querySelectorAll("#from-currency a").forEach((menu) => menu.addEventListener("click", function(){
+    //1. get button
+    //2. change button value
+    document.getElementById("from-button").textContent=this.textContent;
+    //3. store selected currency rate value to the variable
+    fromCurrency = this.textContent;
+    console.log("current currency is", fromCurrency);
+}));
+
+// button action -> click event (for to currency)
+document.querySelectorAll("#to-currency a").forEach((menu) => menu.addEventListener("click", function(){
+    //1. get button
+    //2. change button value
+    document.getElementById("to-button").textContent=this.textContent;
+    //3. store selected currency rate value to the variable
+    toCurrency = this.textContent;
+    console.log("current currency is", toCurrency);
+}));
